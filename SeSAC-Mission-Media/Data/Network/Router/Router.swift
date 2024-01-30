dcad34f54767e83d7d9cf9c6b20f4d8da24c67b8
Taeyoung Won <hexter1994@gmail.com>
2024-01-30 20:03:51 +0900

    [Feat] Router 프로토콜 및 공통 메서드 구현

---
+++ SeSAC-Mission-Media/Data/Network/Router/Router.swift
@@ -0,0 +1,35 @@
+//
+//  Router.swift
+//  SeSAC-Mission-Media
+//
+//  Created by 원태영 on 1/30/24.
+//
+
+import Alamofire
+import Foundation
+
+protocol Router: URLRequestConvertible {
+  
+  var method: HTTPMethod { get }
+  
+  var baseURL: String { get }
+  
+  var path: String { get }
+  
+  var headers: HTTPHeaders { get }
+  
+  var parameters: Parameters? { get }
+}
+
+extension Router {
+  func asURLRequest() throws -> URLRequest {
+    let url: URL = try baseURL.asURL().appendingPathComponent(path)
+    
+    let request = URLRequest(url: url).configured {
+      $0.httpMethod = method.rawValue
+      $0.headers = headers
+    }
+    
+    return try URLEncoding.default.encode(request, with: parameters)
+  }
+}
