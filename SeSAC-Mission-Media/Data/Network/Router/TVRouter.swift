1c25cd30180f8b334877da44cf1e7af24fda3b7a
Taeyoung Won <hexter1994@gmail.com>
2024-01-30 20:04:10 +0900

    [Feat] TV Router 열거형 구현

---
+++ SeSAC-Mission-Media/Data/Network/Router/TVRouter.swift
@@ -0,0 +1,53 @@
+//
+//  TVRouter.swift
+//  SeSAC-Mission-Movie
+//
+//  Created by 원태영 on 1/30/24.
+//
+
+import Alamofire
+import Foundation
+
+enum TVRouter: Router {
+  
+  case topRated
+  case popular
+  
+  var method: HTTPMethod {
+    switch self {
+      case .topRated:
+        return .get
+        
+      case .popular:
+        return .get
+    }
+  }
+  
+  var baseURL: String {
+    return "https://api.themoviedb.org/3/tv"
+  }
+  
+  var path: String {
+    switch self {
+      case .topRated:
+        return "/top_rated"
+        
+      case .popular:
+        return "/popular"
+    }
+  }
+  
+  var headers: HTTPHeaders {
+    return [APIKey.TMDB.authorization.key: APIKey.TMDB.authorization.value]
+  }
+  
+  var parameters: Parameters? {
+    switch self {
+      case .topRated:
+        return nil
+        
+      case .popular:
+        return nil
+    }
+  }
+}
