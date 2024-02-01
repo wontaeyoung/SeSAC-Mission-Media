419588acfe4f32f12323a063f86cc55a73eb02a8
Taeyoung Won <hexter1994@gmail.com>
2024-02-01 21:24:58 +0900

    [Fix] Controllable 프로토콜을 한 파일에서 관리하도록 병합

--- SeSAC-Mission-Media/Presenter/Common/Protocol/UIControllable.swift
+++ SeSAC-Mission-Media/Presenter/Common/Protocol/UIControllable.swift
@@ -1,11 +1,15 @@
 //
-//  CollectionControllable.swift
+//  UIControllable.swift
 //  SeSAC-Mission-Media
 //
-//  Created by 원태영 on 1/30/24.
+//  Created by 원태영 on 2/1/24.
 //
 
 import UIKit
+
+protocol SearchBarControllable: UISearchBarDelegate { }
+
+protocol TableControllable: UITableViewDelegate, UITableViewDataSource { }
 
 protocol CollectionControllable: UICollectionViewDelegate, UICollectionViewDataSource {
   func makeCollectionLayout() -> UICollectionViewFlowLayout
