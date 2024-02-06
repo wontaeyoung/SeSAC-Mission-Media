fa1270fb7f2c81d2b802f4bed1dbbe83d4c1af8e
Taeyoung Won <hexter1994@gmail.com>
2024-02-06 16:54:25 +0900

    [Feat] 배우 모델에 대해 cast와 results 두 케이스를 모두 처리할 수 있도록 디코딩 초기화 로직 구현

--- SeSAC-Mission-Media/Data/Model/Actor.swift
+++ SeSAC-Mission-Media/Data/Model/Actor.swift
@@ -11,11 +11,27 @@ struct ActorResponseDTO: DTO {
   let results: [ActorDTO]
   
   enum CodingKeys: String, CodingKey {
-    case results = "results"
+    case results
+    case cast
   }
   
   func asModel() -> ActorResponse {
     return ActorResponse(results: results.map { $0.asModel() })
+  }
+  
+  /// cast에서 사용하는 배우와 person에서 사용하는 배우의 items 프로퍼티 키가 다른 상황
+  /// 조건 분기로 현재 Response에서 전달한 프로퍼티 키를 찾아서 할당하는 로직 필요
+  init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    
+    /// results 프로퍼티로 디코딩이 가능하지 않으면 -> cast 프로퍼티로 디코딩 시도
+    guard let results = try container.decodeIfPresent([ActorDTO].self, forKey: .results) else {
+      self.results = try container.decodeIfPresent([ActorDTO].self, forKey: .cast) ?? []
+      return
+    }
+    
+    /// results로 성공하면 해당 프로퍼티 할당
+    self.results = results
   }
 }
 
