69e4e6663438bdd52aeca206613a304e4d38b9d2
Taeyoung Won <hexter1994@gmail.com>
2024-02-07 21:11:37 +0900

    [Feat] 프로필 화면 뷰 컨트롤러 구현, 테이블 델리게이트 메서드 구현

--- SeSAC-Mission-Media/Presenter/Main/TabBar/Profile/ProfileViewController.swift
+++ SeSAC-Mission-Media/Presenter/Main/TabBar/Profile/ProfileViewController.swift
@@ -22,30 +22,57 @@ enum ProfileItem: String, CaseIterable {
 final class ProfileViewController: BaseViewController {
   
   // MARK: - UI
+  private let profileView = ProfileView()
   
   
   // MARK: - Property
   weak var coordinator: ProfileCoordinator?
-  
-  // MARK: - Initializer
-  
+  private var profileItemDict: [ProfileItem: String] = [:]
   
   // MARK: - Life Cycle
-  override func setHierarchy() {
-    
+  override func loadView() {
+    self.view = profileView
   }
   
   override func setAttribute() {
-    view.backgroundColor = .systemRed
+    self.profileView.setDelegate(with: self)
+    ProfileItem.allCases.forEach {
+      profileItemDict.updateValue("", forKey: $0)
+    }
   }
-  
-  override func setConstraint() {
-    
-  }
-  
-  
-  // MARK: - Method
-  
-  
 }
 
+extension ProfileViewController: TableControllable {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return ProfileItem.allCases.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let item = ProfileItem.allCases[indexPath.row]
+    
+    let cell = tableView.dequeueReusableCell(
+      withIdentifier: ProfileItemTableViewCell.identifier,
+      for: indexPath
+    ) as! ProfileItemTableViewCell
+    
+    guard let content = profileItemDict[item] else { return cell }
+    
+    cell.setData(with: item.title, content: content)
+    
+    return cell
+  }
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    let item = ProfileItem.allCases[indexPath.row]
+    
+    guard let text = profileItemDict[item] else { return }
+    
+    coordinator?.showUpdateProfileViewController(profileItem: item, currentText: text) { [weak self] in
+      guard let self else { return }
+      
+      profileItemDict.updateValue($0, forKey: item)
+      tableView.reloadRows(at: [indexPath], with: .automatic)
+      coordinator?.pop()
+    }
+  }
+}
